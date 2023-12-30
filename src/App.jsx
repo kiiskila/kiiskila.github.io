54c30e8f008b10daf864b1870a4113c7ba81e95f
Will Kiiskila <kiiskila.will@gmail.com>
2023-12-30 17:33:02 -0600

    use image for background

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,44 @@ function App() {
     },
   };
 
+  const mobileButtonStyle = {
+    ...buttonStyle,
+    fontSize: "sm",
+    px: 3,
+    py: 2,
+  };
+
+  const dividerStyle = {
+    borderColor: "#4dbe7a",
+    borderWidth: "2px",
+    width: "50%",
+    alignSelf: "center",
+  };
+
+  const firstBoxStyle = {
+    flex: isMobile ? "none" : "1",
+    height: isMobile ? "100vh" : "auto",
+    backgroundImage: `url('src/assets/beach.jpg')`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    p: 12,
+    position: "relative",
+    zIndex: 0,
+    _before: {
+      content: '""',
+      position: "absolute",
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      bg: "rgba(44, 45, 51, 0.8)",
+      zIndex: -1,
+    },
+  };
+
   return (
     <ChakraProvider>
       <Box
@@ -32,37 +70,38 @@ function App() {
         flexDirection={isMobile ? "column" : "row"}
         minHeight="100vh"
       >
-        <Box
-          flex={isMobile ? "none" : "1"}
-          height={isMobile ? "100vh" : "auto"}
-          bg="#2c2d33"
-          color="white"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <VStack spacing={4}>
+        <Box sx={firstBoxStyle}>
+          <VStack spacing={4} color="white">
             <Heading fontWeight={300}>WILL KIISKILA</Heading>
-            <Divider />
+            <Divider sx={dividerStyle} />
             <Text fontSize="xl">
               Hi! My name is Will and I&apos;m a computer science graduate
               currently working as a security software developer for Immigrate
               Software. I&apos;m passionate about building software to make life
               easier, and I have an interest in cyber security!
             </Text>
-            <HStack spacing={4}>
+            <HStack spacing={4} pt={2}>
               <Link href="mailto:kiiskila.will@gmail.com" isExternal>
-                <Button leftIcon={<FaEnvelope />} sx={buttonStyle}>
+                <Button
+                  leftIcon={<FaEnvelope />}
+                  sx={isMobile ? mobileButtonStyle : buttonStyle}
+                >
                   Email
                 </Button>
               </Link>
               <Link href="https://www.linkedin.com/in/wkiiskila" isExternal>
-                <Button leftIcon={<FaLinkedin />} sx={buttonStyle}>
+                <Button
+                  leftIcon={<FaLinkedin />}
+                  sx={isMobile ? mobileButtonStyle : buttonStyle}
+                >
                   LinkedIn
                 </Button>
               </Link>
               <Link href="https://github.com/kiiskila" isExternal>
-                <Button leftIcon={<FaGithub />} sx={buttonStyle}>
+                <Button
+                  leftIcon={<FaGithub />}
+                  sx={isMobile ? mobileButtonStyle : buttonStyle}
+                >
                   GitHub
                 </Button>
               </Link>
@@ -76,12 +115,13 @@ function App() {
           display="flex"
           alignItems="center"
           justifyContent="center"
+          p={12}
         >
           <VStack spacing={4}>
             <Heading color="#2c2d33" fontWeight={300}>
               About me
             </Heading>
-            <Divider />
+            <Divider sx={dividerStyle} />
             <Text fontSize="xl" color="#999">
               I’m a computer science graduate from Thompson Rivers University in
               beautiful British Columbia. I am currently working remotely for
