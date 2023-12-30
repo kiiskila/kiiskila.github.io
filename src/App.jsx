94e87d664aac503c3ad26217752481e421675ebd
Will Kiiskila <kiiskila.will@gmail.com>
2023-12-30 16:22:18 -0600

    add default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
 function App() {
+  const isMobile = useBreakpointValue({ base: true, lg: false });
+
   return (
-    <>
-      <h1>Will Kiiskila</h1>
-    </>
+    <ChakraProvider>
+      <Box
+        display="flex"
+        flexDirection={isMobile ? "column" : "row"}
+        minHeight="100vh"
+      >
+        <Box
+          flex={isMobile ? "none" : "1"}
+          height={isMobile ? "100vh" : "auto"}
+          bg="darkgray"
+          color="white"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text fontSize="xl">Lorem ipsum dolor sit amet...</Text>
+        </Box>
+        <Box
+          flex={isMobile ? "none" : "1"}
+          height={isMobile ? "100vh" : "auto"}
+          bg="whitesmoke"
+          color="darkgray"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text fontSize="xl">Consectetur adipiscing elit...</Text>
+        </Box>
+      </Box>
+    </ChakraProvider>
   );
 }
 
