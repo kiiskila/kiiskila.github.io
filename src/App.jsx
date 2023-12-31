e97cfd4b0356e6132f76cd4908728be09c60d701
Will Kiiskila <kiiskila.will@gmail.com>
2023-12-30 18:18:56 -0600

    refactor main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import {
   ChakraProvider,
   Box,
-  Text,
-  useBreakpointValue,
-  Heading,
-  Divider,
   VStack,
+  Divider,
+  Text,
+  Heading,
+  Link,
   Button,
   HStack,
-  Link,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import PropTypes from "prop-types";
 
-function App() {
+const useResponsiveStyles = () => {
   const isMobile = useBreakpointValue({ base: true, lg: false });
+
   const buttonStyle = {
     background: "linear-gradient(to top, #4dbe7a 50%, lightgray 50%)",
     backgroundSize: "100% 200%",
@@ -32,36 +34,58 @@ function App() {
     py: 2,
   };
 
-  const dividerStyle = {
-    borderColor: "#4dbe7a",
-    borderWidth: "2px",
-    width: "50%",
-    //alignSelf: "center",
-  };
-
-  const firstBoxStyle = {
-    flex: isMobile ? "none" : "1",
-    height: isMobile ? "100vh" : "auto",
-    backgroundImage: `url('src/assets/beach.jpg')`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    p: 12,
-    position: "relative",
-    zIndex: 0,
-    _before: {
-      content: '""',
-      position: "absolute",
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      bg: "rgba(44, 45, 51, 0.8)",
-      zIndex: -1,
+  return {
+    isMobile,
+    buttonStyle: isMobile ? mobileButtonStyle : buttonStyle,
+    dividerStyle: {
+      borderColor: "#4dbe7a",
+      borderWidth: "2px",
+      width: "50%",
+    },
+    firstBoxStyle: {
+      flex: isMobile ? "none" : "1",
+      height: isMobile ? "100vh" : "auto",
+      backgroundImage: `url('src/assets/beach.jpg')`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      p: 12,
+      position: "relative",
+      zIndex: 0,
+      _before: {
+        content: '""',
+        position: "absolute",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        bg: "rgba(44, 45, 51, 0.8)",
+        zIndex: -1,
+      },
     },
   };
+};
+
+const ContactButton = ({ href, icon, buttonStyle, children }) => (
+  <Link href={href} isExternal>
+    <Button leftIcon={icon} sx={buttonStyle}>
+      {children}
+    </Button>
+  </Link>
+);
+
+ContactButton.propTypes = {
+  href: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+  buttonStyle: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+function App() {
+  const { isMobile, buttonStyle, dividerStyle, firstBoxStyle } =
+    useResponsiveStyles();
 
   return (
     <ChakraProvider>
@@ -99,30 +123,27 @@ function App() {
               software architecture.
             </Text>
             <HStack spacing={4} pt={2}>
-              <Link href="mailto:kiiskila.will@gmail.com" isExternal>
-                <Button
-                  leftIcon={<FaEnvelope />}
-                  sx={isMobile ? mobileButtonStyle : buttonStyle}
-                >
-                  Email
-                </Button>
-              </Link>
-              <Link href="https://www.linkedin.com/in/wkiiskila" isExternal>
-                <Button
-                  leftIcon={<FaLinkedin />}
-                  sx={isMobile ? mobileButtonStyle : buttonStyle}
-                >
-                  LinkedIn
-                </Button>
-              </Link>
-              <Link href="https://github.com/kiiskila" isExternal>
-                <Button
-                  leftIcon={<FaGithub />}
-                  sx={isMobile ? mobileButtonStyle : buttonStyle}
-                >
-                  GitHub
-                </Button>
-              </Link>
+              <ContactButton
+                href="mailto:kiiskila.will@gmail.com"
+                icon={<FaEnvelope />}
+                buttonStyle={buttonStyle}
+              >
+                Email
+              </ContactButton>
+              <ContactButton
+                href="https://www.linkedin.com/in/wkiiskila"
+                icon={<FaLinkedin />}
+                buttonStyle={buttonStyle}
+              >
+                LinkedIn
+              </ContactButton>
+              <ContactButton
+                href="https://github.com/kiiskila"
+                icon={<FaGithub />}
+                buttonStyle={buttonStyle}
+              >
+                GitHub
+              </ContactButton>
             </HStack>
           </VStack>
         </Box>
@@ -144,7 +165,7 @@ function App() {
               Originally from British Columbia, I now work remotely out of
               Saskatoon as a full-stack developer. My primary focus is on
               building robust web applications using React, TypeScript, PHP
-              (Laravel), and PostgreSQL. I'm also an active contributor to open
+              (Laravel), and PostgreSQL. I am also an active contributor to open
               source, maintaining projects like{" "}
               <Link
                 href="https://www.npmjs.com/package/lorem-ipsum-norwegian"
